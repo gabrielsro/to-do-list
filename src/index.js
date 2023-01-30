0f3aba5c36bb6307ca80cee26d74d15157a3d7ae
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-01-30 17:10:14 -0500

    Created basic development environment via webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const trial = document.createElement("p");
+trial.classList.add("trial");
+trial.innerText = "Created by index.js. Testing live server now. Pretty cool.";
+
+document.body.appendChild(trial);
