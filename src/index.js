383246114dbf2179e90cc58971e8d1f92a03016b
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-01-30 17:27:13 -0500

    testing gh subtree push to gh-pages branch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./style.css";
 
 const trial = document.createElement("p");
 trial.classList.add("trial");
-trial.innerText = "Created by index.js. New test";
+trial.innerText = "Created by index.js. New test for trying out GH veracity";
 
 document.body.appendChild(trial);
